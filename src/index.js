a092eb1ae907fd8949796840695af3e8b8d093a8
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2023-01-16 22:21:05 +0300

    мелкие правки

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { modalForm, modalDocument } from "./modules/modals";
 import { smoothScroll } from "./modules/scroll";
 import { timer } from "./modules/timer";
 
-//! Осталось 7 заданий + доп
+//! Осталось 6 заданий + доп
 modalForm('.button>a', '.header-modal', '.header-modal__close')//! ТЗ-2
 modalForm('.service-button>a', '.services-modal', '.services-modal__close')//! ТЗ-5
 modalDocument('.sertificate-document')//! ТЗ-8
 smoothScroll('.smooth-scroll')//! ТЗ-9
-timer('20 january 2023')
+timer('20 january 2023')//! ТЗ-6
